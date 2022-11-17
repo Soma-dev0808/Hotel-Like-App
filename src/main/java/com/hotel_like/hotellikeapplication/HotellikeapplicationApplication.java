4c7fa77a8915415ba62cace16d60f938256c9139
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-11-16 18:58:34 -0800

    first commit

---
+++ src/main/java/com/hotel_like/hotellikeapplication/HotellikeapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.hotel_like.hotellikeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotellikeapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotellikeapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel_like/hotellikeapplication/HotellikeapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_like.hotellikeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotellikeapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
