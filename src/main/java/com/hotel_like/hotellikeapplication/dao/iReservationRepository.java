9f8f8c73ed73ce83aabdf7ecb8472e4a22531bbe
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-11-22 14:23:55 -0800

    Added get method from reservation

--- src/main/java/com/hotel_like/hotellikeapplication/dao/iReservationRepository.java
+++ src/main/java/com/hotel_like/hotellikeapplication/dao/iReservationRepository.java
@@ -2,21 +2,26 @@ package com.hotel_like.hotellikeapplication.dao;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hotel_like.hotellikeapplication.entity.Reservation;
 
-public interface iReservationRepository  extends CrudRepository<Reservation, Integer>{
-	
-	/**
-	 * get all reservaiton list
-	 */
-	public List<Reservation> getAllReservList();
-	
-	/**
-	 * get a reservaiton information
-	 * @param reservNo
-	 * @param roomType
-	 */
-	Reservation getReservInfo(Integer reservNo, int roomType);
+public interface iReservationRepository extends JpaRepository<Reservation, Integer> {
+
+    /**
+     * get all reservaiton list
+     */
+    // public List<Reservation> getAllReservList();
+
+    @Override
+    public List<Reservation> findAll();
+
+    /**
+     * get a reservaiton information
+     * 
+     * @param reservNo
+     * @param roomType
+     */
+    // Reservation getReservInfo(Integer reservNo, int roomType);
 }
