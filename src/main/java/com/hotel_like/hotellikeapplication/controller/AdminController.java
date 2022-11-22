8e0772b8625bace856c2d032a43eead9dd8e2ee8
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-11-21 17:36:11 -0800

    Added markup for admin page

--- src/main/java/com/hotel_like/hotellikeapplication/controller/AdminController.java
+++ src/main/java/com/hotel_like/hotellikeapplication/controller/AdminController.java
@@ -1,5 +1,34 @@
 package com.hotel_like.hotellikeapplication.controller;
 
-public class AdminController {
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.hotel_like.hotellikeapplication.entity.Reservation;
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin")
+    public String showLogin(Model model) {
+        List<Reservation> reserves = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            reserves.add(
+                    new Reservation(
+                            1,
+                            "test1",
+                            123,
+                            new Date(System.currentTimeMillis()),
+                            2,
+                            1231323,
+                            "asda@dadas.com"));
+        }
+
+        model.addAttribute("reservatoins", reserves);
+        return "admin/admin.html";
+    }
 }
