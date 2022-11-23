e706eea381d6fd960999dc7055aff01b477389e5
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-11-22 20:54:50 -0800

    Added query for reservations list

--- src/main/java/com/hotel_like/hotellikeapplication/controller/AdminController.java
+++ src/main/java/com/hotel_like/hotellikeapplication/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import com.hotel_like.hotellikeapplication.dao.iReservationRepository;
+import com.hotel_like.hotellikeapplication.dto.ReservationInfo;
 import com.hotel_like.hotellikeapplication.entity.Reservation;
 
 @Controller
@@ -22,27 +23,7 @@ public class AdminController {
     @GetMapping("/admin")
     public String showLogin(Model model) {
 
-        List<Reservation> reserves = reservationRepository.findAll();
-
-        System.out.println(reserves);
-
-        // List<Reservation> reserves = new ArrayList<>();
-
-        // for (int i = 1; i <= 10; i++) {
-        // Reservation r = new Reservation(
-        // 1,
-        // "test1",
-        // 123,
-        // new Date(System.currentTimeMillis()),
-        // 2,
-        // 1231323,
-        // "asda@dadas.com");
-
-        // r.setReservNo(i);
-
-        // reserves.add(r);
-
-        // }
+        List<ReservationInfo> reserves = reservationRepository.getAllReservList();
 
         model.addAttribute("reservatoins", reserves);
         return "admin/admin.html";
--- src/main/java/com/hotel_like/hotellikeapplication/dao/iReservationRepository.java
+++ src/main/java/com/hotel_like/hotellikeapplication/dao/iReservationRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 // import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.hotel_like.hotellikeapplication.dto.ReservationInfo;
 import com.hotel_like.hotellikeapplication.entity.Reservation;
 
 public interface iReservationRepository extends JpaRepository<Reservation, Integer> {
@@ -12,10 +14,13 @@ public interface iReservationRepository extends JpaRepository<Reservation, Integ
     /**
      * get all reservaiton list
      */
-    // public List<Reservation> getAllReservList();
-
-    @Override
-    public List<Reservation> findAll();
+    @Query(nativeQuery = true, value = "SELECT r.reserv_no as reservNo, r.room_type as roomType, " +
+            "r.reserv_name as reservName, r.user_id as userId, r.start_date as startDate, r.end_date as endDate," +
+            "r.stay_people as stayPeople, r.phone_number as phoneNumber, r.email as email, rm.room_name as roomName " +
+            "FROM reservation r " +
+            "LEFT JOIN room rm " +
+            "ON r.room_type = rm.room_type;")
+    public List<ReservationInfo> getAllReservList();
 
     /**
      * get a reservaiton information
