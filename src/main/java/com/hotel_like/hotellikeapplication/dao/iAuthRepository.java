b7386d50e91eb051e4e6e4bd9701988e9f60ab89
Yuya Minamide <dettu1003@gmail.com>
2022-11-24 21:56:25 -0800

    add login and signup function

--- src/main/java/com/hotel_like/hotellikeapplication/dao/iAuthRepository.java
+++ src/main/java/com/hotel_like/hotellikeapplication/dao/iAuthRepository.java
@@ -1,16 +1,13 @@
 package com.hotel_like.hotellikeapplication.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.hotel_like.hotellikeapplication.entity.User;
 
-public interface iAuthRepository extends CrudRepository<User, Integer> {
-
+public interface iAuthRepository extends JpaRepository<User, Integer>{
+	
 	/**
 	 * get user account by email and password
-	 * 
 	 * @param email
 	 * @param password
 	 */
-	// User findUser(String email, String password);
+	User findUserByEmailAndPassword(String email, Integer password);
 }
--- src/main/java/com/hotel_like/hotellikeapplication/entity/User.java
+++ src/main/java/com/hotel_like/hotellikeapplication/entity/User.java
@@ -1,99 +1,93 @@
 package com.hotel_like.hotellikeapplication.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name="\"user\"")
 public class User {
 	@Id
 	private int userId;
-	private String userType;
-	private int password;
-	private String email;
+    private String userType;
+    private int password;
+    private String email;
 
-	public User() {
-	}
+    public User() {
+    }
 
-	public User(
-			int userId,
-			String userType,
-			int password,
-			String email) {
-		this.userType = userType;
-		this.password = password;
-		this.email = email;
-	}
-
-	/**
-	 * getter : userId
-	 * 
-	 * @return userId
-	 */
+    public User(
+    		int userId, 
+    		String userType, 
+    		int password, 
+    		String email) {
+    	this.userType = userType;
+    	this.password = password;
+    	this.email = email;
+    }
+    
+    /**
+     * getter : userId
+     * @return userId
+     */
 	public int getUserId() {
 		return userId;
 	}
 
 	/**
-	 * setter : userId
-	 * 
-	 * @param userId
-	 */
+     * setter : userId
+     * @param userId
+     */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
 
 	/**
-	 * getter : userType
-	 * 
-	 * @return userType
-	 */
+     * getter : userType
+     * @return userType
+     */
 	public String getUserType() {
 		return userType;
 	}
 
 	/**
-	 * setter : userType
-	 * 
-	 * @param userType
-	 */
+     * setter : userType
+     * @param userType
+     */
 	public void setUserType(String userType) {
 		this.userType = userType;
 	}
 
 	/**
-	 * getter : password
-	 * 
-	 * @return password
-	 */
+     * getter : password
+     * @return password
+     */
 	public int getPassword() {
 		return password;
 	}
 
 	/**
-	 * setter : password
-	 * 
-	 * @param password
-	 */
+     * setter : password
+     * @param password
+     */
 	public void setPassword(int password) {
 		this.password = password;
 	}
 
 	/**
-	 * getter : email
-	 * 
-	 * @return email
-	 */
+     * getter : email
+     * @return email
+     */
 	public String getEmail() {
 		return email;
 	}
 
 	/**
-	 * setter : email
-	 * 
-	 * @param email
-	 */
+     * setter : email
+     * @param email
+     */
 	public void setEmail(String email) {
 		this.email = email;
 	}
